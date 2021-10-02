fe783b9ec2b7ba6e007cc64b16fe8b0ab1124755
unknown <hreyes.condesa@gmail.com>
2021-10-01 22:56:35 -0300

    Primera fase de TDD. Implementación de clase ProjectRepositoryTest (RED).

---
+++ OBLDA2/DataAccess/Repositories/ProjectRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using DataAccessInterface;
+using Domain;
+using System;
+
+namespace DataAccess.Repositories
+{
+    public class ProjectRepository : Repository<Project, Guid>, IProjectRepository
+    {
+        private readonly DbSet<Project> _DbSet;
+        private readonly DbContext _context;
+
+        public ProjectRepository(DbContext context) : base(context)
+        {
+            this._context = context;
+            this._DbSet = context.Set<Project>();
+        }
+
+        public List<Project> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
