2d78d55aea83376eef53213b2e172b607c12423a
unknown <hreyes.condesa@gmail.com>
2021-10-03 00:43:44 -0300

    Segunda fase de TDD. Implementación de clase TesterController.

--- OBLDA2/WebApi/Controllers/TesterController.cs
+++ OBLDA2/WebApi/Controllers/TesterController.cs
@@ -19,6 +19,48 @@ namespace WebApi.Controllers
             this.testerLogic = testerLogic;
         }
 
+        [HttpGet]
+        public IActionResult GetAllBugs([FromBody] Guid id)
+        {
+            User user = new User();
+            user.Id = id;
+
+            IEnumerable<Bug> bugs = this.testerLogic.GetAllBugs(user);
+
+            return (StatusCode((int)HttpStatusCode.OK, bugs));
+
+        }
+
+        [HttpPost]
+        public IActionResult AssignTester([FromBody] ProjectEntryModel project, [FromBody] UserEntryModel tester)
+        {
+            try
+            {
+                testerLogic.AssignTesterToProject(project.ToEntity(), tester.ToEntity());
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return NotFound("Project not found or Tester not found");
+            }
+
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTester(Project project, User tester)
+        {
+            try
+            {
+                testerLogic.DeleteTesterInProject(project, tester);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return NotFound("Project not found or Tester not found");
+            }
+
+
+        }
 
     }
 }
--- OBLDA2/WebApiTest/TesterControllerTest.cs
+++ OBLDA2/WebApiTest/TesterControllerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using BusinessLogicInterface;
@@ -16,15 +17,10 @@ namespace WebApiTest
     {
         private static State activeState = new State(State.active);
 
-        private User admin;
         private User tester;
-        private User developer;
+        private Rol rolTester;
         private Project project;
         private Bug bug;
-
-        private Rol rolAdministrator;
-        private Rol rolTester;
-        private Rol rolDeveloper;
 
         private Mock<ITesterLogic> testerLogic;
 
@@ -32,15 +28,13 @@ namespace WebApiTest
         [TestInitialize]
         public void Setup()
         {
-            rolAdministrator = new Rol(Rol.administrator);
             rolTester = new Rol( Rol.tester);
-
             tester = new User("Juan", "Gomez", "jgomez", "admin1234", "gomez@gmail.com", rolTester);
-            developer = new User("Diego", "Suarez", "diegoo", "admin1234", "diegoo@gmail.com", rolDeveloper);
-            bug = new Bug(project, 1, "Error de login", "Intento de sesión", "3.0", activeState);
 
+            bug = new Bug(project, 1, "Error de login", "Intento de sesión", "3.0", activeState);
             project = new Project("Project - GXC ");
 
+            testerLogic = new Mock<ITesterLogic>(MockBehavior.Strict);
         }
 
 
@@ -49,14 +43,13 @@ namespace WebApiTest
         {
             ProjectEntryModel projectEntryModel = new ProjectEntryModel(project);
             UserEntryModel testerEntryModel = new UserEntryModel(tester);
-            var testerLogic = new Mock<ITesterLogic>(MockBehavior.Strict);
+
             testerLogic.Setup(x => x.AssignTesterToProject(project, tester));
 
             var controller = new TesterController(testerLogic.Object);
             var result = controller.AssignTester(projectEntryModel, testerEntryModel);
             var status = result as NoContentResult;
 
-            testerLogic.VerifyAll();
             Assert.AreEqual(204, status.StatusCode);
 
         }
@@ -64,31 +57,26 @@ namespace WebApiTest
         [TestMethod]
         public void DeleteTesterTest()
         {
-            var testerLogic = new Mock<ITesterLogic>(MockBehavior.Strict);
-
             testerLogic.Setup(m => m.DeleteTesterInProject(project, tester));
 
             var controller = new TesterController(testerLogic.Object);
 
             IActionResult result = controller.DeleteTester(project, tester);
-            var status = result as StatusCodeResult;
+            var status = result as NoContentResult;
 
             testerLogic.VerifyAll();
-            Assert.AreEqual(200, status.StatusCode); // aca lo mismo yo tiro un 200,  ver si esta bien???
+            Assert.AreEqual(204, status.StatusCode);
         }
 
 
         [TestMethod]
         public void GetTotalBugsTest()
         {
-
-            var testerLogic = new Mock<ITesterLogic>(MockBehavior.Strict);
-
             List<Bug> list = new List<Bug>();
             list.Add(bug);
 
             testerLogic.Setup(m => m.GetAllBugs(tester)).Returns(list);
-            var controller = new TesterController(testerLogic.Object);
+            TesterController controller = new TesterController(testerLogic.Object);
 
             var result = controller.GetAllBugs(tester.Id);
             var okResult = result as OkObjectResult;
@@ -97,7 +85,6 @@ namespace WebApiTest
             testerLogic.VerifyAll();
 
             Assert.IsTrue(list.SequenceEqual(bugsResult));
-
         }
         
     }
