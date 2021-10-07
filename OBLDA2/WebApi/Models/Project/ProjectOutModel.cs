aa97a441c726c408508556d0f4dd4cea27eb1391
unknown <hreyes.condesa@gmail.com>
2021-10-06 22:08:33 -0300

    Arreglo de error sobre muestro de usuarios

--- OBLDA2/WebApi/Models/Project/ProjectOutModel.cs
+++ OBLDA2/WebApi/Models/Project/ProjectOutModel.cs
@@ -8,8 +8,8 @@ namespace OBLDA2.Models
     public class ProjectOutModel
     {
         public Guid Id { get; set; }
-        public IEnumerable<UserOutModel> Users { get; set; }
-        public IEnumerable<BugEntryOutModel> Bugs { get; set; }
+        public List<UserEntryModel> Users { get; set; }
+        public List<BugEntryOutModel> Bugs { get; set; }
 
         public int TotalBugs { get; set; }
 
@@ -20,10 +20,26 @@ namespace OBLDA2.Models
         public ProjectOutModel(Project project)
         {
             this.Id = project.Id;
-            this.Users = project.Users.Select(u => new UserOutModel(u));
-            this.Bugs = project.Bugs.Select(b => new BugEntryOutModel(b));
+
+
             this.TotalBugs = project.TotalBugs;
             this.Name = project.Name;
+
+            this.Users = new List<UserEntryModel>();
+
+            if(project.Users!=null)
+            foreach (User user in project.Users)
+            {
+                this.Users.Add(new UserEntryModel(user));
+            }
+
+            this.Bugs = new List<BugEntryOutModel>();
+
+            if(project.Bugs != null)
+            foreach (Bug bug in project.Bugs)
+            {
+                this.Bugs.Add(new BugEntryOutModel(bug));
+            }
         }
 
     }
