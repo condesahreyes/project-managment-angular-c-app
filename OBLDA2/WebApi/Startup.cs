60e5ad64557dc026bbba5849f81f80c1af7e375f
unknown <hreyes.condesa@gmail.com>
2021-10-13 21:42:34 -0300

    Correción para poder conectarse a nuestra web api con puertos cruzados

--- OBLDA2/WebApi/Startup.cs
+++ OBLDA2/WebApi/Startup.cs
@@ -22,6 +22,17 @@ namespace OBLDA2
         {
             services.AddControllers();
 
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllHeaders",
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "OBLDA2", Version = "v1" });
@@ -39,6 +50,8 @@ namespace OBLDA2
                 options.SerializerSettings.ReferenceLoopHandling
                 = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
+
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -48,13 +61,14 @@ namespace OBLDA2
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OBLDA2 v1"));
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors("AllowAllHeaders");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
