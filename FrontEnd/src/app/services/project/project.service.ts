398ba31f3db914dc2ce5cb33d3eac3b0adc61297
DiegoAsadurian <diegoasadurian@gmail.com>
2021-10-27 10:30:30 -0300

    Refactor

--- FrontEnd/src/app/services/project/project.service.ts
+++ FrontEnd/src/app/services/project/project.service.ts
@@ -39,7 +39,8 @@ export class ProjectService {
   }
 
   updateProject(idProject: string, project: Project): Observable<Project> {
-    return this.http.put<Project>(`${this.uri}/${idProject}`, {
+    console.log("el id es " + idProject)
+    return this.http.put<Project>( this.uri + "/" + idProject, {
       Name: project.Name
     });//.pipe(catchError(this.handlerError.handleError));
   }
